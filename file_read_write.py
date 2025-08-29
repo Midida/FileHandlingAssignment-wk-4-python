f101718aa7302a4fc90bc0cfbae22c77b3c954a9
ERICK MIDIDA <mididaerick88@gmail.com>
2025-08-29 09:00:53 +0300

    Update file_read_write.py

--- file_read_write.py
+++ file_read_write.py
@@ -15,18 +15,3 @@ try:
 except FileNotFoundError:
     print("Error: input.txt not found. Please make sure the file exists.")
 
-# error_handling_lab.py
-
-filename = input("Enter the filename you want to read: ")
-
-try:
-    with open(filename, "r") as file:
-        data = file.read()
-        print("\nFile content:\n")
-        print(data)
-except FileNotFoundError:
-    print(f"Error: The file '{filename}' does not exist.")
-except PermissionError:
-    print(f"Error: You do not have permission to read '{filename}'.")
-except Exception as e:
-    print(f"An unexpected error occurred: {e}")
